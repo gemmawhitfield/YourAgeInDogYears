62b91955d18ee75dd57f66dde7fa464212d602de
Gemma Whitfield <70214774+gemmawhitfield@users.noreply.github.com>
2021-03-01 12:54:42 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// My age variable
+let myAge = 25;
+
+// The first two years of my life
+let earlyYears = 2;
+
+// Convert my first two years to a dog's first two years
+earlyYears *= 10.5;
+
+// My age minus 2
+let laterYears = myAge - 2;
+
+laterYears *= 4; 
+
+//Add early years and later years together
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Holly';
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years!`);
